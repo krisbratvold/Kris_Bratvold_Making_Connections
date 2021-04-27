b044e4a7acee05df1bc953ca11a9d3f571bed602
krisbratvold <kris.bratvold@gmail.com>
2021-04-27 14:31:24 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var requests = document.querySelector(".requests");
+var requests2 = document.querySelector(".requests2")
+var count = 2;
+
+
+function remove(){
+    var myElement = document.querySelector("span");
+    requests.remove();
+    count--;
+    myElement.innerHTML = count;
+}
+
+
+function remove2(){
+    var myElement = document.querySelector("span");
+    requests2.remove();
+    count--;
+    myElement.innerHTML = count;
+}
+
+
+function showbox(){
+    nameEdit.style.display = "inline-block";
+}
+
+function editName(){
+    userName.innerHTML=nameEdit.value;
+}
